dc9e53c152ed7d14e4a9883948c20f7399786a2b
Scott Wisniewski <scott@agile.security>
2026-02-16 13:35:16 -0800

    Fixup script.

--- home/scott/admin_connect.sh
+++ home/scott/admin_connect.sh
@@ -5,4 +5,4 @@ export PGPASSWORD=$(aws dsql generate-db-connect-admin-auth-token \
 
 #PGPASSWORD=$(aws dsql generate-db-connect-auth-token --hostname tmabtw3svuq5o33rhy7wvhlyai.dsql.us-east-2.on.aws --region us-east-2)
 
-PGSSLMODE=require psql --dbname postgres --username admin --host aiabui4qj4tzepgdw5sf5yc264.dsql.us-east-2.on.aws $*
+PGSSLMODE=require psql --dbname postgres --username admin --host aiabui4qj4tzepgdw5sf5yc264.dsql.us-east-2.on.aws "$@"
